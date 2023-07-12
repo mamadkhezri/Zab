17b792fd209222ef61292e4493e0f87f3a1f43c4
mamadkhezri <138501134+mamadkhezri@users.noreply.github.com>
2023-07-12 20:17:19 +0200

    make posts model and register

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -10,15 +11,22 @@ class Post(models.Model):
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
-    upload = models.FileField(upload_to='blog/')
+    upload = models.FileField(upload_to='media/blog/')
     #tags = 
     #category = models.ManyToManyField(Category)
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
-    slug = models.SlugField(max_length=100, null=True, blank=True)
+    slug = models.SlugField()
     login_require = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+
+
+    def __str__(self):
+        return self.title
+        
+
 
 
