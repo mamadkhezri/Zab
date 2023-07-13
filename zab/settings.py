76d3e664ec09892bfeb6778409c2e858d1c89abb
mamadkhezri <138501134+mamadkhezri@users.noreply.github.com>
2023-07-13 17:06:21 +0200

    end of modify index.html

--- zab/settings.py
+++ zab/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,10 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 MEDIA_URL = '/media/'
 
+MEDIA_ROOT = os.path.join (BASE_DIR, 'media')
 
-MEDIA_ROOT = (BASE_DIR, 'media')
 
 
 
