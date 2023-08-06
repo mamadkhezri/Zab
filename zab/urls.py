fa30e1460859613b43b8f0426295a00abee159c4
mamadkhezri <138501134+mamadkhezri@users.noreply.github.com>
2023-08-06 12:47:28 +0200

    modify root urls

--- zab/urls.py
+++ zab/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path ('posts', include('posts.urls')),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
